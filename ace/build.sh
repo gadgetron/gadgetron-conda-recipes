b2a4623261b9c706c57f47d98cfc83c303f5496f
Souheil Inati <souheil.inati@nih.gov>
2015-07-15 13:35:52 -0400

    Fixed ACE on the mac.

--- ace/build.sh
+++ ace/build.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
 export ACE_ROOT=`pwd`
-export LD_LIBRARY_PATH=$ACE_ROOT/lib:$LD_LIBRARY_PATH
 
 mkdir $PREFIX/lib
 
 if [ `uname` == Darwin ]; then
+    export DYLD_LIBRARY_PATH=$ACE_ROOT/ace:$DYLD_LIBRARY_PATH
     # How do we know what system we are on?
     echo "#include \"ace/config-macosx-mavericks.h\"" > ace/config.h
-    echo "INSTALL_PREFIX = $PREFIX" > include/makeinclude/platform_macros.GNU 
-    echo "include \$(ACE_ROOT)/include/makeinclude/platform_macosx_mavericks.GNU" >> include/makeinclude/platform_macros.GNU 
-    make -j$(sysctl -n hw.ncpu) ACE
-    make -j$(sysctl -n hw.ncpu) install
+    echo "include \$(ACE_ROOT)/include/makeinclude/platform_macosx_mavericks.GNU" > include/makeinclude/platform_macros.GNU 
+    make -C ace -f GNUmakefile.ACE INSTALL_PREFIX=$PREFIX LDFLAGS="" DESTDIR="" INST_DIR="/ace" debug=0 shared_libs=1 static_libs=0 install
 fi
 
 if [ `uname` == Linux ]; then
+    export LD_LIBRARY_PATH=$ACE_ROOT/lib:$LD_LIBRARY_PATH
     echo "#include \"ace/config-linux.h\"" > ace/config.h
     echo "INSTALL_PREFIX = $PREFIX" > include/makeinclude/platform_macros.GNU 
     echo "include \$(ACE_ROOT)/include/makeinclude/platform_linux.GNU" >> include/makeinclude/platform_macros.GNU 
     CFLAGS="-fPIC"
-    make -j${CPU_COUNT} ACE
-    make  -j${CPU_COUNT} install
+    make ACE
+    make install
 fi
 
 
