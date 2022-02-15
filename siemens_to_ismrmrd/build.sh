37cbca7744bf0966790a9918fae80fe01ea1d6ec
V. R <for_usenet@yahoo.com>
2022-02-15 09:41:22 -0500

    Updating Siemens to ISMRMRD converter in manner similar to how ISMRMRD was modified to build on macOS. Adding preprocessing selectors for platform-specific compilers.

--- siemens_to_ismrmrd/build.sh
+++ siemens_to_ismrmrd/build.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 mkdir -p build
 cd build
-cmake -G Ninja -DBUILD_DYNAMIC=ON -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
+cmake -G Ninja -DBUILD_DYNAMIC=ON -DCMAKE_C_COMPILER=$CONDA_PREFIX/bin/clang -DCMAKE_CXX_COMPILER=$CONDA_PREFIX/bin/clang++ -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
 ninja
 ninja install
