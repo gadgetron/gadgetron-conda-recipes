2a5b0e6f87a01d1f5e290b2ced50ca1e6223a812
V. R <for_usenet@yahoo.com>
2022-02-16 09:14:51 -0500

    Remove specification of clang compiler in build scripts, and this doesn't work on linux. Use conda environment activate capabilities to set these variables.

--- ismrmrd/build.sh
+++ ismrmrd/build.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 mkdir -p build
 cd build
-cmake -GNinja -DCMAKE_C_COMPILER=$CONDA_PREFIX/bin/clang -DCMAKE_CXX_COMPILER=$CONDA_PREFIX/bin/clang++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
 ninja
 ninja install
