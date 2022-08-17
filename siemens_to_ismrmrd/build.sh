42da66364a84e07654b5065ffa1b942f22390d99
Michael Hansen <mihansen@microsoft.com>
2022-08-17 01:05:44 +0000

    Removed packages from ISMRMRD org

--- siemens_to_ismrmrd/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-mkdir -p build
-cd build
-cmake -G Ninja -DBUILD_DYNAMIC=ON -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
-ninja
-ninja install
