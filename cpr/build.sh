3c64112472848b1b8ccc7aa044c2b18a1a1e9399
V. R <for_usenet@yahoo.com>
2022-08-31 10:26:12 -0400

    Testing to see if forcing use of system curl when building cpr for macOS is resulting in non-relocatable packages.

--- cpr/build.sh
+++ cpr/build.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 
 mkdir -p build
 cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCPR_FORCE_USE_SYSTEM_CURL=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
+if [[ $(uname) =~ Linux ]]; then
+   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCPR_FORCE_USE_SYSTEM_CURL=ON  -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
+else
+   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCPR_FORCE_USE_SYSTEM_CURL=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
+fi
 ninja
 ninja install
