c758de722c8075a4c6b2e13e0b2c3354b1556e4b
V. R <for_usenet@yahoo.com>
2022-08-31 11:15:13 -0400

    Reflecting removal of most support packages in main build script.

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ Options:
 EOF
 }
 
-all_packages=("ismrmrd" "ismrmrd-python" "mrd-storage-server" "siemens_to_ismrmrd" "range-v3" "cpr" "gadgetron-python")
+all_packages=("mrd-storage-server" "range-v3" "cpr")
 packages_to_build=()
 user="gadgetron"
 
