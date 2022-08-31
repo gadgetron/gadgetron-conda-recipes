5145b5fe32c00c57b885709018e29609825517c4
V. R <for_usenet@yahoo.com>
2022-08-31 11:46:35 -0400

    Revert build configuration, but keep use of system curl OFF on macOS.

--- cpr/build.sh
+++ cpr/build.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 mkdir -p build
 cd build
-if [[ $(uname) =~ Darwin ]]; then
-   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCPR_FORCE_USE_SYSTEM_CURL=OFF -CMAKE_C_COMPILER=${CONDA_PREFIX}/bin/clang -CMAKE_CXX_COMPILER=${CONDA_PREFIX}/bin/clang++ -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
-else
+if [[ $(uname) =~ Linux ]]; then
    cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCPR_FORCE_USE_SYSTEM_CURL=ON  -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
+else
+   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCPR_FORCE_USE_SYSTEM_CURL=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
 fi
 ninja
 ninja install
