815eb0ab40db40910d35ff898dcc3f83ffe51bc9
Michael Hansen <mihansen@microsoft.com>
2022-09-01 20:54:39 +0200

    Remove packages from ISMRMRD org (#36)
    * Removed packages from ISMRMRD org
    
    * Moved to matrix build
    
    * Removed gadgetron-python
    
    * Updated README

--- ismrmrd/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-mkdir -p build
-cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
-ninja
-ninja install
--- mrd-storage-server/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-go build -o "${PREFIX}/bin/mrd-storage-server" .
